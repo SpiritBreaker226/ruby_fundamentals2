1197fd62ea77fdb309b8bdfeb0fb55822a79492a
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-21 14:55:50 -0500

    Display the second item on the food array list

--- exercise6.rb
+++ exercise6.rb
@@ -14,3 +14,4 @@ puts "Number of Groceries on the list: #{grocery_list.count}"
 
 puts grocery_list.index("bananas") == nil ?  "You don't need to pick up bananas today" : "You need to pick up bananas"
 
+puts "The second item on the list is " << grocery_list[1]
