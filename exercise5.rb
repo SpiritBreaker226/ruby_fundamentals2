1d8a488eba830c4967b535402548b9c0111e7e69
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-21 14:07:53 -0500

    Add in text asking about the temperature and displaying the results

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,6 @@
 def to_celsius(fahrenheit)
 	((fahrenheit.to_i - 32) * 5/9)
 end
+
+puts "What is the temperture in Fahrenheit?"
+puts "The current temperture is #{to_celsius(gets.chomp)} in Celsius"
