b85126bad26e6844bea81e0de092553e582e5d4b
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-21 15:15:05 -0500

    Update display method in exercise 6 to have a more menafully name, have an optional starting charecter and new line after each line to better display the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,16 @@
-def display_food_array(food_array)
-	food_array.each { |food| puts "* #{food}" }
+def display_grocery_list(grocery_list, strating_charecter = "")
+	grocery_list.each { |food| puts "#{strating_charecter} #{food}" }
+
+	puts "\n"
 end
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-display_food_array(grocery_list)
+display_grocery_list(grocery_list, "*")
 
 grocery_list << "rice"
 
-display_food_array(grocery_list)
+display_grocery_list(grocery_list)
 
 puts "Number of Groceries on the list: #{grocery_list.count}"
 
@@ -16,6 +18,6 @@ puts grocery_list.index("bananas") == nil ?  "You don't need to pick up bananas 
 
 puts "The second item on the list is " << grocery_list[1]
 
-display_food_array(grocery_list.sort!)
+display_grocery_list(grocery_list.sort!, "*")
 
-display_food_array(grocery_list.delete_if { |food| food == "salmon" })
+display_grocery_list(grocery_list.delete_if { |food| food == "salmon" })
