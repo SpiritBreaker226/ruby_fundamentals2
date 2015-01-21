e97978d93fbb60ab37c003103da3f46b8aa8704b
Jason Stathopulos <jstathopulos@gmail.com>
2015-01-21 15:07:42 -0500

    Remove Salmon from the food array list

--- exercise6.rb
+++ exercise6.rb
@@ -17,3 +17,5 @@ puts grocery_list.index("bananas") == nil ?  "You don't need to pick up bananas 
 puts "The second item on the list is " << grocery_list[1]
 
 display_food_array(grocery_list.sort!)
+
+display_food_array(grocery_list.delete_if { |food| food == "salmon" })
